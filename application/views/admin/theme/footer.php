5773bd73829e325e25c66b308e63fc93854cb177
niloydey9.com <dev.niloydey@gmail.com>
2023-05-03 12:34:30 +0600

    Dashbord design customization

---
+++ application/views/admin/theme/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+  <div class="footer-left">
+    <p>Copyright © 2023 <b>BDchatbots</b>. All Rights Reserved.</p>
+  </div>
+  <div class="footer-right">
+
+  </div>
+
+  
+</footer>
